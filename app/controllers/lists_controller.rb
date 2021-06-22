4e12a1b663f1f0bc8a291bdc90d04e31108eb4dc
Kym hatran <kym.hatran@gmail.com>
2021-06-22 19:17:40 +1000

    watch-list

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,43 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+
+    redirect_to lists_path(list)
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmarks = @list.bookmarks
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    @list.update(params[:list]) # do you need a form?
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+
+    redirect_to lists_path(lists)
+  end
+
+  private
+
+  def list_params
+    params(:list).require(:name)
+  end
+end
